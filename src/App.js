2105dec395bc329c0bae088317a6cc8c638d87de
Greg Sirko <greg.sirko@icloud.com>
2026-09-16 23:17:38 -0500

    Update todo app

--- src/App.js
+++ src/App.js
@@ -1,56 +1,62 @@
-import {useState} from 'react';
-import AddTaskForm from './components/AddTaskForm.jsx';
-import UpdateForm from './components/UpdateForm.jsx';
-import ToDo from './components/ToDo.jsx';
+import { useState, useEffect } from "react";
+import AddTaskForm from "./components/AddTaskForm.jsx";
+import UpdateForm from "./components/UpdateForm.jsx";
+import ToDo from "./components/ToDo.jsx";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import './App.css';
+import "./App.css";
 
 function App() {
-
   // Tasks (ToDo List) State
-  const [toDo, setToDo] = useState([]);
+  const [toDo, setToDo] = useState(() => {
+    const savedTasks = localStorage.getItem("toDo");
+    return savedTasks ? JSON.parse(savedTasks) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("toDo", JSON.stringify(toDo));
+  }, [toDo]);
 
   // Temp State
-  const [newTask, setNewTask] = useState('');
-  const [updateData, setUpdateData] = useState('');
+  const [newTask, setNewTask] = useState("");
+  const [updateData, setUpdateData] = useState("");
 
-  // Add task 
+  // Add task
   ///////////////////////////
   const addTask = () => {
-    if(newTask) {
-      let num = toDo.length + 1; 
-      let newEntry = { id: num, title: newTask, status: false }
-      setToDo([...toDo, newEntry])
-      setNewTask('');
+    if (newTask) {
+      let num = toDo.length + 1;
+      let newEntry = { id: num, title: newTask, status: false };
+      setToDo([...toDo, newEntry]);
+      setNewTask("");
     }
-  }
+  };
 
-  // Delete task 
+  // Delete task
   ///////////////////////////
   const deleteTask = (id) => {
-    let newTasks = toDo.filter( task => task.id !== id)
+    let newTasks = toDo.filter((task) => task.id !== id);
     setToDo(newTasks);
-  }
+  };
 
   // Mark task as done or completed
   ///////////////////////////
   const markDone = (id) => {
-    let newTask = toDo.map( task => {
-      if( task.id === id ) {
-        return ({ ...task, status: !task.status })
+    let newTask = toDo.map((task) => {
+      if (task.id === id) {
+        return { ...task, status: !task.status };
       }
       return task;
-    })
+    });
     setToDo(newTask);
-  }
+  };
 
   // Cancel update
   ///////////////////////////
   const cancelUpdate = () => {
-    setUpdateData('');
-  }
+    setUpdateData("");
+  };
 
   // Change task for update
   ///////////////////////////
@@ -58,53 +64,53 @@ function App() {
     let newEntry = {
       id: updateData.id,
       title: e.target.value,
-      status: updateData.status ? true : false
-    }
+      status: updateData.status ? true : false,
+    };
     setUpdateData(newEntry);
-  }
+  };
 
   // Update task
   ///////////////////////////
   const updateTask = () => {
-    let filterRecords = [...toDo].filter( task => task.id !== updateData.id );
-    let updatedObject = [...filterRecords, updateData]
+    let filterRecords = [...toDo].filter((task) => task.id !== updateData.id);
+    let updatedObject = [...filterRecords, updateData];
     setToDo(updatedObject);
-    setUpdateData('');
-  }
+    setUpdateData("");
+  };
 
   return (
     <div className="container App">
+      <br />
+      <br />
+      <h2>To Do List App (ReactJS)</h2>
+      <br />
+      <br />
 
-    <br /><br />
-    <h2>To Do List App (ReactJS)</h2>
-    <br /><br />
+      {updateData && updateData ? (
+        <UpdateForm
+          updateData={updateData}
+          changeTask={changeTask}
+          updateTask={updateTask}
+          cancelUpdate={cancelUpdate}
+        />
+      ) : (
+        <AddTaskForm
+          newTask={newTask}
+          setNewTask={setNewTask}
+          addTask={addTask}
+        />
+      )}
 
-    {updateData && updateData ? (
-      <UpdateForm 
-        updateData={updateData}
-        changeTask={changeTask}
-        updateTask={updateTask}
-        cancelUpdate={cancelUpdate}
+      {/* Display ToDos */}
+
+      {toDo && toDo.length ? "" : "No Tasks..."}
+
+      <ToDo
+        toDo={toDo}
+        markDone={markDone}
+        setUpdateData={setUpdateData}
+        deleteTask={deleteTask}
       />
-    ) : (
-      <AddTaskForm 
-        newTask={newTask}
-        setNewTask={setNewTask}
-        addTask={addTask}
-      />
-    )}
-
-    {/* Display ToDos */}
-
-    {toDo && toDo.length ? '' : 'No Tasks...'}
-
-    <ToDo
-      toDo={toDo}
-      markDone={markDone}
-      setUpdateData={setUpdateData}
-      deleteTask={deleteTask}
-    />  
-
     </div>
   );
 }
